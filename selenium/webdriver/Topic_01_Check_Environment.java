28488c3248e4bdb70655b9dca52b026a8d6b22b2
quynhlien710 <quynhlien710@gmail.com>
2023-11-07 15:16:49 +0700

    Add first commit

---
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -0,0 +1,44 @@
+package webdriver;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_01_Check_Environment {
+
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://www.facebook.com/");
+	}
+
+	@Test
+	public void TC_01_Url() {
+		Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/");
+	}
+
+	@Test
+	public void TC_02_Logo() {
+		Assert.assertTrue(driver.findElement(By.cssSelector("img.fb_logo")).isDisplayed());
+	}
+
+	@Test
+	public void TC_03_Form() {
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
